75e5186bd4eca0ad2d07a8f4e3cb3c7769e55996
azkgar <16088140+azkgar@users.noreply.github.com>
2026-08-09 18:35:03 -0400

    Chapter 1 - Mixed Messages

--- cards.c
+++ cards.c
@@ -10,41 +10,45 @@
  int main()
  {
     char card_name[3];
-
-    puts("Enter the card_name: ");
-
-    scanf("%2s", card_name);
-
-    int val = 0;
     int count = 0;
-    
-    switch(card_name[0])
-    {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-            break;
-    }
 
-    printf("The card value is: %i\n", val);
+    do
+    {
+        int val = 0;
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
 
-    if ((val >= 3) && (val <= 6))
-    {
-        count++;
-        puts("Count has gone up");
-    }
-    else if (val == 10)
-    {
-        count--;
-        puts("Count has gone down");
-    }
+        switch(card_name[0])
+        {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10)) 
+                {
+                    puts("Wrong input. Try again");
+                }
+                break;
+        }
+
+        if((val >= 3) && (val <= 6))
+        {
+            count++;
+        }
+        else if (val == 10)
+        {
+            count--;
+        }
+
+        printf("Current count: %i\n", count);
+
+    } while(card_name[0] != 'X');
 
     return 0;
  }
